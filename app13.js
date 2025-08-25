9fe79a020c14bee69ba642fbe98fccb08ffb04b8
Shiva69-prasad <shiva31694@gmail.com>
2025-08-25 23:45:01 +0530

    app13.js

---
+++ app13.js
@@ -0,0 +1,35 @@
+//Scopes
+
+ //1.global scope
+
+function a(){
+  const b="this is shiva coding"
+  console.log(b)
+}
+a()
+
+OR 
+
+const fruits="papaya"
+
+function shiva(){
+    const message="hii";
+    const fruits="grapes"
+    console.log(message)
+    console.log(fruits)
+
+}
+
+OR
+
+function santhu(){
+     const message="good morning"
+   console.log(message)
+}
+shiva()
+santhu()
+
+
+
+
+
